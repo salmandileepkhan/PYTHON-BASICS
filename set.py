0c16a34a40ac441bd3202d4417f2e38c21ba3084
SALMAN DILEEPKHAN <79751861+salmandileepkhan@users.noreply.github.com>
2022-10-30 12:09:25 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,35 @@
+
+Code
+set1
+set1={'a','b''c'}
+set1
+{'a', 'bc'}
+1
+
+type(set1)
+set
+
+set cannot be indexed
+set2
+set2={1,2,3,4,5,6}
+set2
+{1, 2, 3, 4, 5, 6}
+
+support 1)union 2)intersection 3) Difference
+
+set1={1,2,3,4,5}
+set2={3,4,5,6,7}
+set1.union(set2)
+{1, 2, 3, 4, 5, 6, 7}
+set2
+
+set1.intersection(set2)
+{3, 4, 5}
+et2
+
+set1.difference(set2)
+{1, 2}
+set1
+
+set2.issubset(set1)
+False
