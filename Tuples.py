6d211955dff4f94e07a05cf3548a636c658c05ce
SALMAN DILEEPKHAN <79751861+salmandileepkhan@users.noreply.github.com>
2022-10-30 11:47:49 +0530

    Create Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,53 @@
+View
+Insert
+Cell
+Kernel
+Help
+
+Code
+tup1=1,2,3,4,5
+tup1
+tup1=1,2,3,4,5
+tup1
+(1, 2, 3, 4, 5)
+tup1
+type(tup1)
+tuple
+=250
+tup1[0]=250
+---------------------------------------------------------------------------
+TypeError                                 Traceback (most recent call last)
+Cell In [3], line 1
+----> 1 tup1[0]=250
+
+TypeError: 'tuple' object does not support item assignment
+
+p2
+tup2=(1,)
+type(tup2)
+tuple
+tup2
+tup2
+(1,)
+('a','b')+('c','d')
+('a', 'b', 'c', 'd')
+*3
+('a','b','c')*3
+('a', 'b', 'c', 'a', 'b', 'c', 'a', 'b', 'c')
+sal
+
+sal=(1,2,3,4,5)
+max(sal)
+5
+
+st=[1,2,3]
+nd=['a','b','c']
+for i in zip(st,nd):
+    print(i)
+(1, 'a')
+(2, 'b')
+(3, 'c')
+
+sal=('a','b','c','a')
+sal.count('a')
+2
